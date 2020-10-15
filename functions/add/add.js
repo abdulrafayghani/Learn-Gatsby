30fba9a99b8978c13fa06770c5418489e3a53fca
abdulrafayghani <abdulrafay.ghani@gmail.com>
2020-10-15 15:24:01 +0500

    apollo lambda

--- functions/add/add.js
+++ functions/add/add.js
@@ -1,33 +1,33 @@
-    // const faunadb = require('faunadb'),
-    //     q = faunadb.query;
-    //  require('dotenv').config()
-    //  q = faunadb.query
+    const faunadb = require('faunadb'),
+        q = faunadb.query;
+     require('dotenv').config()
+     q = faunadb.query
 
-    //  exports.handler = async (event, context ) => {
-    //     try{
-    //         if(event.httpMethod !== "POST"){
-    //             return{
-    //                 statusCode: 405, 
-    //                 body: "Method is not allowed"
-    //             }
-    //         }
+     exports.handler = async (event, context ) => {
+        try{
+            if(event.httpMethod !== "POST"){
+                return{
+                    statusCode: 405, 
+                    body: "Method is not allowed"
+                }
+            }
 
-    //         let reqObj = JSON.parse(event.body)
+            let reqObj = JSON.parse(event.body)
 
-    //         var client = new faunadb.Client({ secret: process.env.FAUNADB_ADMIN_SECRET })
+            var client = new faunadb.Client({ secret: process.env.FAUNADB_ADMIN_SECRET })
 
-    //         var result = await client.query(
-    //             q.Create(
-    //                 q.Collection('directory'),
-    //                 {data: { firstName: reqObj.firstName, lastName: reqObj.lastName, age: reqObj.age }}
-    //             )
-    //         )
+            var result = await client.query(
+                q.Create(
+                    q.Collection('directory'),
+                    {data: { firstName: reqObj.firstName, lastName: reqObj.lastName, age: reqObj.age }}
+                )
+            )
 
-    //         return {
-    //             statusCode: 200, 
-    //             body: JSON.stringify({ id: `${result.ref.id}` })
-    //         }
-    //     } catch (err) {
-    //         return { statusCode: 400, body: err.toString() }
-    //     }
-    //  }
+            return {
+                statusCode: 200, 
+                body: JSON.stringify({ id: `${result.ref.id}` })
+            }
+        } catch (err) {
+            return { statusCode: 400, body: err.toString() }
+        }
+     }
